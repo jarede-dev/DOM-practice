1324a1606ec7ce7f7b3393a69cc0a5a82c320a8e
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-17 08:55:11 +0800

    added gray bgcolor, red p text, and blue H3 text

---
+++ script.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundColor = "#D3D3D3";
+
+const redText = document.createElement("p");
+redText.textContent = "Hey, I'm red!";
+redText.style.color = "red";
+
+document.body.appendChild(redText);
+
+const blueH3 = document.createElement("h3");
+blueH3.textContent = "I'm a blue H3!";
+blueH3.style.color = "blue";
+
+document.body.appendChild(blueH3);
