8a950f34716884d78d27660ae1a28f7bae74e1b7
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-17 09:10:08 +0800

    created div, added h1 and another p in the div. added styles in the div

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ blueH3.textContent = "I'm a blue H3!";
 blueH3.style.color = "blue";
 
 document.body.appendChild(blueH3);
+
+const div = document.createElement("div");
+div.setAttribute("style", "border-style: solid; border-color: black; background: pink; padding: 20px; width: 200px");
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+const p = document.createElement("p");
+p.textContent = "ME TOO!";
+
+div.appendChild(h1);
+div.appendChild(p);
+
+document.body.appendChild(div);
